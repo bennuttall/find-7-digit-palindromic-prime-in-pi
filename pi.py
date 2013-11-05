2985bdf117973451989c567a1746e0fe3ba69496
Ben Nuttall <ben@bennuttall.com>
2013-11-05 01:44:22 +0000

    Convert tabs to spaces, add spaces around operators, add main method

--- pi.py
+++ pi.py
@@ -1,25 +1,28 @@
 def isprime(n):
-    n*=1.0
-    if n%2==0 and n!=2 or n%3==0 and n!=3:
+    n *= 1.0
+    if n %2 == 0 and n != 2 or n % 3 == 0 and n != 3:
         return False
-    for b in range(1,int((n**0.5+1)/6.0+1)):
-        if n%(6*b-1)==0:
+    for b in range(1, int((n ** 0.5 + 1) / 6.0 + 1)):
+        if n % (6 * b - 1) == 0:
             return False
-        if n %(6*b+1)==0:
+        if n % (6 * b + 1) == 0:
            return False
     return True
 
-f = open('pi.txt','r')
+def main():
+    f = open('pi.txt', 'r')
+    d = f.readline(7)
+    
+    while True:
+        n = f.readline(1)
+        if len(n):
+            d = d[1:7] + n
+            if d == d[::-1]:
+                if isprime(int(d)):
+                    print d
+                    break  # remove this break to see more solutions
+        else:
+            break
 
-d = f.readline(7)
-
-while 1:
-	n = f.readline(1)
-	if len(n):
-		d = d[1:7] + n
-		if d == d[::-1]:
-			if isprime(int(d)):
-				print d
-				break # remove this break to see more solutions
-	else:
-		break
+if __name__ == '__main__':
+    main()
